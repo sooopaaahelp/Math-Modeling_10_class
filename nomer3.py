bde89a9c124232da3af215435f461f6c27e9d25d
sooopaaahelp <56309224+sooopaaahelp@users.noreply.github.com>
2019-11-12 17:40:24 +0200

    lab6

--- nomer3.py
+++ nomer3.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation 
 fig,ax=plt.subplots()
-anime,=plt.plot([],[],marker='0', color='r')
+anime,=plt.plot([],[],marker='o', color='r')
 def cikl(R,t):
     x=R*(t-np.sin(t))
     y=R*(1-np.cos(t))
@@ -11,14 +11,14 @@ def astr (R, t):
     x=R*np.cos(t)**3
     y=R*np.sin(t)**3
     return x,y
-ax.set_xlim(-5,100)
-ax.set_ylim(-10,100)
+ax.set_xlim(-2*np.pi,2*np.pi)
+ax.set_ylim(-2*np.pi,2*np.pi)
 def animat(i):
-    anime.set_data(astr(1, i))
-    anime.set_data(cikl(1, i))
+    anime.set_data(astr(3, i))
+
 ani=FuncAnimation(fig,
                   animat,
-                  frames=100,
+                  frames=np.arange(-2*np.pi,2*np.pi,0.1),
                   interval=300)
 ani.save('anime_0.gif')
         
